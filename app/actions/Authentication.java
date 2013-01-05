37d7123202be9ce20794dbbee03d9261af597088
Cameron Hunter <hello@cameronhunter.co.uk>
2013-01-05 15:42:20 -0800

    Fixing null pointed

--- app/actions/Authentication.java
+++ app/actions/Authentication.java
@@ -3,13 +3,15 @@ package actions;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import play.mvc.Action;
 import play.mvc.Http.Context;
+import play.mvc.Http.Cookie;
 import play.mvc.Result;
 
 public class Authentication extends Action.Simple {
 
     @Override
     public Result call( Context context ) throws Throwable {
-        if ( isNotBlank( context.request().cookies().get( "oauthtoken" ).value() ) ) {
+        Cookie cookie = context.request().cookies().get( "oauthtoken" );
+        if ( cookie != null && isNotBlank( cookie.value() ) ) {
             return delegate.call( context );
         }
         return redirect( "/" );
--- app/actions/HomeIfAuthenticated.java
+++ app/actions/HomeIfAuthenticated.java
@@ -1,16 +1,17 @@
 package actions;
 
-import org.apache.commons.lang3.StringUtils;
-
+import static org.apache.commons.lang3.StringUtils.isBlank;
 import play.mvc.Action;
 import play.mvc.Http.Context;
+import play.mvc.Http.Cookie;
 import play.mvc.Result;
 
 public class HomeIfAuthenticated extends Action.Simple {
 
     @Override
     public Result call( Context context ) throws Throwable {
-        if (StringUtils.isBlank( context.request().cookies().get( "oauthtoken" ).value() ) ) {
+        Cookie cookie = context.request().cookies().get( "oauthtoken" );
+        if ( cookie == null || isBlank( cookie.value() ) ) {
             return delegate.call( context );
         }
         return redirect( "/home" );
